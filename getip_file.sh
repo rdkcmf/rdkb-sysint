b4baf2c953c22b46c7d6f2e8aa03c8ee3ecfc688
Balaji Punnuru <balaji_punnuru@cable.comcast.com>
2017-04-18 00:12:26 +0000

    RDKB-10720: Fixed runtime issues with container.
    Reason for change: Fixed issues found during running the
                       container which is created in readonly
                       root file system.
    
    Test Procedure: Build and Verify.
    
    Risks: Low.
    
    Change-Id: I34e79ff557b0184d969cd23bef882cfa7b448fee
    Signed-off-by: Balaji Punnuru <balaji_punnuru@cable.comcast.com>

--- getip_file.sh
+++ getip_file.sh
@@ -1,3 +1,4 @@
-#!/bin/sh -
+#!/bin/sh
 
-/sbin/ifconfig eth0 | /bin/grep 'inet addr:' | /usr/bin/cut -d: -f2 | /usr/bin/awk '{ print "CONTAINER_LIGHTTPD_IP="\$1}' > /tmp/ip_file.sh
+/sbin/ifconfig eth0 | /bin/grep 'inet addr:' | /usr/bin/cut -d: -f2 | /usr/bin/awk '{ print "CONTAINER_LIGHTTPD_IP="$1}' > /tmp/container_env.sh
+touch /tmp/ip_file.sh
