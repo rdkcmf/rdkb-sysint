1d4568bab0c7eb8547eff87a9ccf60047c353966
kiran biswal <kiran_biswal@cable.comcast.com>
2017-06-23 05:48:52 +0000

    RDKB-11649: telemetry logs for was
    Reason for change:added 24 hour cron
    
    Test Procedure: on atom
    
    check if wifihealth.txt has the below prints everyday (once per day) depending on was is enabled/disabled/pin/pbc configured or not
    
    170623-05:44:06.570437 RDKB_WPS_ENABLED_1:TRUE
    170623-05:44:06.574807 RDKB_WPS_ENABLED_2:TRUE
    170623-05:44:06.579259 RDKB_WPS_PBC_CONFIGURED_1:TRUE
    170623-05:44:06.583889 RDKB_WPS_PBC_CONFIGURED_2:TRUE
    170623-05:44:06.591912 RDKB_WPS_PIN_CONFIGURED_1:FALSE
    170623-05:44:06.596308 RDKB_WPS_PIN_CONFIGURED_2:FALSE
    
    Change-Id: I92c038a694044ec2df9cb877ed0f6f0f94dc8799
    Risks: Low
    Signed-off-by: Kiran Biswal<kiran_biswal@cable.comcast.com>

--- log_mem_cpu_info_atom.sh
+++ log_mem_cpu_info_atom.sh
@@ -21,7 +21,7 @@
 uptime=`cat /proc/uptime | awk '{ print $1 }' | cut -d"." -f1`
 echo "before running log_mem_cpu_info_atom.sh.sh printing top output" 
 top -n1 >> /rdklogs/logs/AtomConsolelog.txt
-if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ] ; then
+if [ $uptime -gt 1800 ] && [ "$(pidof CcspWifiSsp)" != "" ] && [ "$(pidof apup)" == "" ] && [ "$(pidof fastdown)" == "" ] && [ "$(pidof apdown)" == "" ]  && [ "$(pidof aphealth.sh)" == "" ] && [ "$(pidof stahealth.sh)"  == "" ] && [ "$(pidof radiohealth.sh)" == "" ] && [ "$(pidof aphealth_log.sh)" == "" ] && [ "$(pidof bandsteering.sh)" == "" ] && [ "$(pidof l2shealth_log.sh)" == "" ] && [ "$(pidof l2shealth.sh)" == "" ] && [ "$(pidof dailystats_log.sh)" == "" ] && [ "$(pidof dailystats.sh)" == "" ]; then
 	if [ -e /rdklogger/log_capture_path_atom.sh ]
 	then
 		source /rdklogger/log_capture_path_atom.sh 
