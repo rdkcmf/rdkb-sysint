9a1154d79e5b45e3c01f7fbbcd0365a1e908932a
dravin004c <Dhanya_Ravindran2@comcast.com>
2018-01-04 12:27:01 +0000

    RDKB-14318,ARRISXB3-6730 : lighttpd fails to launch sometimes
    Reason for change: Changing the port numbers 51515 and 58081 as there is a possibility for these ports to be
    allocated to some other process as they fall in the local port range which OS can allocate for random ports.
    
    Test Procedure: "Lighttpd is not running" prints should not be present in Arm and Atom.
    
    Risks: None
    Signed-off-by: Dhanya_Ravindran2@comcast.com
    
    (cherry picked from commit 0cb900da2a43c7746567990310187a879acab262)
    (cherry picked from commit 111dbc98c27e001ab3f4f112f122a6c53661007d)
    
    Change-Id: I2a30ce1be8cfdf4c495914e2624af9c1ca26a22b

--- iptables_container.sh
+++ iptables_container.sh
@@ -32,8 +32,8 @@ if [ "x$CONTAINER_LIGHTTPD_IP" != "x" ]; then
 /usr/sbin/iptables -t nat -A PREROUTING -i eth0.4090 -p tcp -d 192.168.251.254 --dport 80 -m conntrack --ctstate NEW  -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:80
 /usr/sbin/iptables -t nat -A PREROUTING -i $LXC_BRIDGE_NAME -p tcp -d 192.168.251.254 --dport 80 -m conntrack --ctstate NEW -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:80
 /usr/sbin/iptables -t nat -A OUTPUT -p tcp -d 192.168.251.254 -m conntrack --ctstate NEW --dport 80 -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:80
-/usr/sbin/iptables -t nat -A PREROUTING -i eth0.4090 -p tcp -d 192.168.251.254 --dport 51515 -m conntrack --ctstate NEW  -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:51515
-/usr/sbin/iptables -t nat -A PREROUTING -i $LXC_BRIDGE_NAME -p tcp -d 192.168.251.254 --dport 51515 -m conntrack --ctstate NEW -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:51515
-/usr/sbin/iptables -t nat -A OUTPUT -p tcp -d 192.168.251.254 -m conntrack --ctstate NEW --dport 51515 -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:51515
+/usr/sbin/iptables -t nat -A PREROUTING -i eth0.4090 -p tcp -d 192.168.251.254 --dport 21515 -m conntrack --ctstate NEW  -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:21515
+/usr/sbin/iptables -t nat -A PREROUTING -i $LXC_BRIDGE_NAME -p tcp -d 192.168.251.254 --dport 21515 -m conntrack --ctstate NEW -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:21515
+/usr/sbin/iptables -t nat -A OUTPUT -p tcp -d 192.168.251.254 -m conntrack --ctstate NEW --dport 21515 -j DNAT --to-destination $CONTAINER_LIGHTTPD_IP:21515
 /usr/sbin/iptables -t nat -A POSTROUTING --out-interface eth0.500 -j MASQUERADE
 fi
