fd435c449d4e19dac544bbf403e4e129adb27822
jjoy202 <Jismon_Joy@cable.comcast.com>
2018-11-08 15:49:16 -0500

    RDKB-19949 : Override partner_id for all partner specific builds
    Reason for change: Adding new script to apply partner_ID from device.properties
    Test Procedure:
    Verify "apply_partner_customization.sh" present under /lib/rdk/
    Risks: None.
    Signed-off-by: Jismon_Joy@cable.comcast.com
    
    (cherry picked from commit 29f36c1c11b2b057213a8f58a9ab8b83fc7c44a4)
    
    Change-Id: Id9113b8aefcc5548d2cb3396a5d966b93f64b402

---
+++ apply_partner_customization.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ -f "/etc/log_timestamp.sh" ];then
+	source /etc/log_timestamp.sh
+fi
+
+CONSOLE_LOG_FILE="/rdklogs/logs/Consolelog.txt.0"
+
+echo_t "Partner customization - START" >> "$CONSOLE_LOG_FILE"
+
+if [ -f "/etc/device.properties" ];then
+	#For partner-specifc builds we have to use the PARTNER_ID from device.properties.
+	partner_id=`cat /etc/device.properties | grep PARTNER_ID | cut -f2 -d=`
+	if [ "$partner_id" != "" ];then
+		echo_t "Partner customization - applying partner_id from device.properties: $partner_id " >> "$CONSOLE_LOG_FILE"
+		echo_t "Partner customization - updating /nvram/.partner_ID with value: $partner_id " >> "$CONSOLE_LOG_FILE"
+		echo $partner_id > /nvram/.partner_ID
+		echo_t "Partner customization - updating syscfg.db with value: $partner_id " >> "$CONSOLE_LOG_FILE"
+		syscfg set PartnerID "$partner_id"
+		syscfg commit
+		sync
+		echo_t "Partner customization - COMPLETED" >> "$CONSOLE_LOG_FILE"
+	else
+		echo_t "Partner customization - FAILED. PartnerID is null from device.properties" >> "$CONSOLE_LOG_FILE"
+	fi
+else
+	echo_t "Partner customization - FAILED. device.properties is missing" >> "$CONSOLE_LOG_FILE"
+fi
