c7cf9de141ad5c7a8d2ebfd8e785e91e8fbba6b4
pkappe316 <Prakash_KAPPESANNANAGAPPA@comcast.com>
2019-10-23 21:51:37 +0000

    RDKB-24991: SNMPv3 unreachable until reboot.
    Reason for change: To overcome retry logic and  RF outage.
    Test Procedure: Test snmpv3 commands
    Risks: Low.
    
    Signed-off-by: Prakash_KAPPESANNANAGAPPA@comcast.com
    (cherry picked from commit 0a53ffff9dd29f38b75ccffd2d5d42d5d058ba86)
    (cherry picked from commit 0c0b4025d733a3d964b288abdec528e7146b6de5)
    
    Change-Id: Iaa0ee84f2c4b537025e840d76ac6059ff150dd98

---
+++ handlesnmpv3.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+#get wan status event
+WAN_STATUS=`sysevent get wan-status`
+SNMPV3=`syscfg get V3Support`
+
+if [ "$WAN_STATUS" = "started" ] && [ "$SNMPV3" = "true" ]; then
+    systemctl restart snmpd.service
+fi
