ec9bf9a588c071462baf9766b4082a27546d0102
pkappe316 <Prakash_KAPPESANNANAGAPPA@comcast.com>
2020-06-11 22:18:04 +0000

    RDKB-29707: Create /tmp/* files based on OCSP RFC's.
    Reason for change: Create /tmp/* files based on OCSP RFC's.
    Test Procedure: Test ocsp files present in /tmp/
    connection.
    Risks: Low.
    
    Signed-off-by: Prakash_KAPPESANNANAGAPPA@comcast.com
    
    RDKB-29710: Add --cert-status option to existing CURL commands RDKV.
    
    Reason for change: add cert-status option to verify cert status.
    Test Procedure: Test all the curl commands to establish secure TLS
    connection.
    Risks: Low.
    
    Signed-off-by: Prakash_KAPPESANNANAGAPPA@comcast.com
    (cherry picked from commit d7ab0821664baa6594d581789047711415f6f2ec)
    
    RDKB-30169: cert validation is not being done in XB3 platforms in ATOM side.
    
    Reason for change: getting rfc via dmcli.
    Test Procedure: Test ocsp.
    connection.
    Risks: Low.
    
    Change-Id: I7afe58528322e05983e4c1b9d7bb151b1b633fdc
    Signed-off-by: Prakash_KAPPESANNANAGAPPA@comcast.com
    (cherry picked from commit 21f30b7743df2f2ac401d5e93b8e78088389c579)
    
    RDKB-30377: Telemetry upload query not having cert-status in curl command.
    
    Reason for change: one of the curl command doesnt have cert-status option
    for telemetry upload query, when codebig is enabled because logging done
    based on sed, just moved cert-status option begining.
    Test Procedure: Test telemetry curl commands by enabling codebig.
    Risks: Low.
    
    Change-Id: I477c8c8369ef8106489178f755c218212790ae97
    Signed-off-by: Prakash_KAPPESANNANAGAPPA@comcast.com
    (cherry picked from commit d526ad029526ab4c120cd7eb86c04f27b89953ab)

---
+++ ocsp-support.sh
@@ -0,0 +1,58 @@
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+if [ -f /etc/device.properties ];then
+    . /etc/device.properties
+fi
+
+if [ -f /etc/include.properties ];then
+    . /etc/include.properties
+fi
+
+OCSP_LOG_FILE="$LOG_PATH/ocsp-support.log"
+StatusOCSPSTAPLE=`syscfg get EnableOCSPStapling`
+if [ -z "$StatusOCSPSTAPLE" ]; then
+    StatusOCSPSTAPLE=`dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.CRL.Enable | grep value | awk '{print $5}'`
+fi
+
+StatusOCSPCA=`syscfg get EnableOCSPCA`
+if [ -z "$StatusOCSPCA" ]; then
+    StatusOCSPCA=`dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.CRL.DirectOCSP | grep value | awk '{print $5}'`
+fi
+
+
+echo "status of RFC StatusOCSPSTAPLE $StatusOCSPSTAPLE StatusOCSPCA $StatusOCSPCA" >> $OCSP_LOG_FILE
+
+if [ "$StatusOCSPSTAPLE" = "true" ]; then
+    echo "[OCSPSTAPLE] Enabled" >> $OCSP_LOG_FILE
+    touch /tmp/.EnableOCSPStapling
+else
+    echo "[OCSPSTAPLE] Disabled" >> $OCSP_LOG_FILE
+    if [ -f /tmp/.EnableOCSPStapling ]; then
+        rm -f /tmp/.EnableOCSPStapling
+    fi
+fi
+
+if [ "$StatusOCSPCA" = "true" ]; then
+    echo "[OCSPCA] Enabled" >> $OCSP_LOG_FILE
+    touch /tmp/.EnableOCSPCA
+else
+    echo "[OCSPCA] Disabled" >> $OCSP_LOG_FILE
+    if [ -f /tmp/.EnableOCSPCA ]; then
+        rm -f /tmp/.EnableOCSPCA
+    fi
+fi
