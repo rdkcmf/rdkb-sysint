244cede820dcbb053fbae5908b69b73231684a82
csaisu662 <Chenchireddygari_SaiSujithKumarReddy@comcast.com>
2021-09-21 06:10:01 +0000

    RDKB-35419:Integrate xpki certs for SHORTS
    Reason for change: Replace RDKCA with xpki certs for SHORTS.
    Test Procedure: Verify SHORTS connectivity to CPE.
    Risks: Medium.
    
    Signed-off-by: csaisu662 <Chenchireddygari_SaiSujithKumarReddy@comcast.com>
    
    RDKB-37121: Enable reverse SSH over SHORTS
    
    Reason for change: To run revssh over Stunnel
    Test Procedure: Build and verify revssh connectivity
    Risks: Medium
    
    Change-Id: I8de53082f2379e3406f2634cf206e3985de4f0e9
    Signed-off-by: csaisu662 <Chenchireddygari_SaiSujithKumarReddy@comcast.com>
    (cherry picked from commit 99f16724e5503986a7218ffa6cf1b46ed2b1c08a)

--- startStunnel.sh
+++ startStunnel.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-
+. /etc/include.properties
+. /etc/device.properties
+. /usr/bin/stunnelCertUtil.sh
 source /etc/log_timestamp.sh
 
 export TERM=xterm
 export HOME=/home/root
-LOG_FILE=/tmp/shortsLog.txt
+LOG_FILE="$LOG_PATH/stunnel.log"
 
 usage()
 {
-  echo_t "USAGE:  startSTunnel.sh <ip_ver> <localip> <jumpserverip> <jumpserverport> <remoteTerminalRows> <remoteTerminalColumns>"
+  echo_t "STUNNEL USAGE:  startSTunnel.sh <localport> <jumpfqdn> <umpserver> <jumpserverport> <reverseSSHArgs>"
 }
 
-if [ $# -lt 6 ]; then
+if [ $# -lt 5 ]; then
    usage
    exit 1
 fi
@@ -25,58 +27,57 @@ if [ -f "/lib/rdk/shortsDownload.sh" ]; then
     fi
 fi
 
-#prepare things
-K_DIR="/tmp/eaqafxwah"
-S_FILE="/etc/ssl/certs/eaqafxwah.vmk"
-D_FILE="/tmp/eaqafxwah/eaqafxwah.vmk"
-mkdir -p /tmp/eaqafxwah
-configparamgen jx $S_FILE $D_FILE
+if [ $DEVICE_TYPE == "broadband" ]; then
+    DEVICE_CERT_PATH=/nvram/certs
+elif [ $DEVICE_TYPE == "mediaclient" -o $DEVICE_TYPE == "hybrid" ]; then
+    DEVICE_CERT_PATH=/opt/certs
+else
+    echo_t "STUNNEL: $DEVICE_CERT_PATH, not expected"
+    DEVICE_CERT_PATH=/tmp/certs
+fi
 
 #collect the arguments
-IP_VER=$1
-LOCAL_IP=$2
+#    1) CPE's available port starting from 3000
+#    2) FQDN of jump server
+#    3) Port number of stunnel's server instance at jump server
+LOCAL_PORT=$1
+JUMP_FQDN=$2
 JUMP_SERVER=$3
 JUMP_PORT=$4
-ROWS=$5
-COLUMNS=$6
+REVERSESSHARGS=$5
 
 #RDM parameters
 DNLD_SCRIPT=/lib/rdk/shortsDownload.sh
 SOCAT_PATH=/tmp/socat_dnld/usr/bin/socat
 STUNNEL_PATH=/tmp/stunnel_dnld/usr/bin/stunnel
 
-#echo "got command $IP_VER $LOCAL_IP $JUMP_SERVER $JUMP_PORT $ROWS $COLUMNS" > /tmp/webpa_command
 
-# there is no harm in using the same port as jump server as 
-# interfaces are different and it was gnerated randmol anyway
-
-STUNNEL_PID_FILE=/tmp/stunnel_pid_$JUMP_PORT.pid
-STUNNEL_CONF_FILE=/tmp/stunnel_$JUMP_PORT.conf
+STUNNEL_PID_FILE=/tmp/stunnel_$LOCAL_PORT.pid
+REVSSH_PID_FILE=/var/tmp/rssh.pid
+STUNNEL_CONF_FILE=/tmp/stunnel_$LOCAL_PORT.conf
 
 echo  "pid = $STUNNEL_PID_FILE"           > $STUNNEL_CONF_FILE
-#echo "output=/tmp/cpe_tunnelssh.log"    >> $STUNNEL_CONF_FILE
-#echo "debug = 7"                        >> $STUNNEL_CONF_FILE
+echo "output=$LOG_FILE"   		 >> $STUNNEL_CONF_FILE
+echo "debug = 7"                       	 >> $STUNNEL_CONF_FILE
 echo  "[ssh]"                            >> $STUNNEL_CONF_FILE
 echo  "client = yes"                     >> $STUNNEL_CONF_FILE
 
-# keep the arguments in ip ver format for clarity 
-# just ipv6 might work as in most cases a bind to ipv6 includes ipv4 for most cases
-if [ $IP_VER -eq 4 ] 
-then
-    echo "accept = 127.0.0.1:$JUMP_PORT"     >> $STUNNEL_CONF_FILE
-    echo "connect = $JUMP_SERVER:$JUMP_PORT" >> $STUNNEL_CONF_FILE
-else
-    echo "accept = ::1:$JUMP_PORT"           >> $STUNNEL_CONF_FILE
-    echo "connect = $JUMP_SERVER:$JUMP_PORT" >> $STUNNEL_CONF_FILE
+# Use localhost to listen on both IPv4 and IPv6
+echo "accept = localhost:$LOCAL_PORT"     >> $STUNNEL_CONF_FILE
+echo "connect = $JUMP_SERVER:$JUMP_PORT" >> $STUNNEL_CONF_FILE
+
+extract_stunnel_client_cert
+
+if [ ! -f $CERT_FILE -o ! -f $CA_FILE ]; then
+    echo_t "STUNNEL: Required cert/CA file not found. Exiting..." >> $LOG_FILE
+    exit 1
 fi
 
-
 # this might change once we get proper certificates
-echo "key = $D_FILE"                                                  >> $STUNNEL_CONF_FILE
-echo "cert = /etc/ssl/certs/device_tls_cert.pem"                      >> $STUNNEL_CONF_FILE
-echo "CAfile =/etc/ssl/certs/comcast-rdk-revshell-server-ca.cert.pem" >> $STUNNEL_CONF_FILE
+echo "cert = $CERT_FILE"                     			      >> $STUNNEL_CONF_FILE
+echo "CAfile = $CA_FILE"                                              >> $STUNNEL_CONF_FILE
 echo "verifyChain = yes"                                              >> $STUNNEL_CONF_FILE
-echo "checkHost = $JUMP_SERVER"                                       >> $STUNNEL_CONF_FILE 
+echo "checkHost = $JUMP_FQDN"                                         >> $STUNNEL_CONF_FILE
 
 if [ -f "/usr/bin/stunnel" ]; then
     /usr/bin/stunnel $STUNNEL_CONF_FILE
@@ -98,21 +99,39 @@ else
     fi
 fi
 
-if [ -f "/usr/bin/socat" ]; then
-    SOCAT_PATH=/usr/bin/socat
-fi
-
 # cleanup sensitive files early
 rm -f $STUNNEL_CONF_FILE
 rm -f $D_FILE
 
-if [ $IP_VER -eq 4 ] 
-then
-    $SOCAT_PATH -w rows=$ROWS columns=$COLUMNS exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:127.0.0.1:$JUMP_PORT
-else
-    $SOCAT_PATH -w rows=$ROWS columns=$COLUMNS exec:'bash -li',pty,stderr,setsid,sigint,sane tcp6:[::1]:$JUMP_PORT
+REVSSHPID1=`cat $REVSSH_PID_FILE`
+STUNNELPID=`cat $STUNNEL_PID_FILE`
+
+if [ -z "$STUNNELPID" ]; then
+    rm -f $STUNNEL_PID_FILE
+    echo_t "STUNNEL: stunnel-client failed to establish. Exiting..." >> $LOG_FILE
+    exit
 fi
 
-stunnel_pid=`cat $STUNNEL_PID_FILE`
-kill $stunnel_pid
+#Starting startTunnel
+/bin/sh /lib/rdk/startTunnel.sh start $REVERSESSHARGS
 
+REVSSHPID2=`cat $REVSSH_PID_FILE`
+
+#Terminate stunnel if revssh fails.
+if [ -z "$REVSSHPID2" ] || [ "$REVSSHPID1" == "$REVSSHPID2" ]; then
+    kill -9 $STUNNELPID
+    rm -f $STUNNEL_PID_FILE
+    echo_t "STUNNEL: Reverse SSH failed to connect. Exiting..." >> $LOG_FILE
+    exit
+fi
+
+echo_t "STUNNEL: Reverse SSH pid = $REVSSHPID2, Stunnel pid = $STUNNELPID" >> $LOG_FILE
+
+#watch for termination of ssh-client to terminate stunnel
+while test -d "/proc/$REVSSHPID2"; do
+     sleep 5
+done
+
+echo_t "STUNNEL: Reverse SSH session ended. Exiting..." >> $LOG_FILE
+kill -9 $STUNNELPID
+rm -f $STUNNEL_PID_FILE
